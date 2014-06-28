5406957f5c63f0d61be27bfb9a9a549345268f32
changun <changun.tw@gmail.com>
2014-06-28 16:02:20 -0400

    resolve moves segment overlapping issue

--- src/main/java/org/ohmage/lifestreams/tasks/moves/FilterDuplicatedSegment.java
+++ src/main/java/org/ohmage/lifestreams/tasks/moves/FilterDuplicatedSegment.java
@@ -1,6 +1,8 @@
 package org.ohmage.lifestreams.tasks.moves;
 
 import co.nutrino.api.moves.impl.dto.storyline.MovesSegment;
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
 import org.ohmage.lifestreams.models.StreamRecord;
 import org.ohmage.lifestreams.tasks.SimpleTask;
 import org.springframework.stereotype.Component;
@@ -9,17 +11,27 @@ public class FilterDuplicatedSegment extends SimpleTask<MovesSegment>{
 	private MovesSegment lastSegment;
 	@Override
 	public void executeDataPoint(StreamRecord<MovesSegment> record) {
+        MovesSegment curSegment = record.getData();
 		if(lastSegment != null){
-			if(!lastSegment.getStartTime().equals(record.getData().getStartTime())){
-				// we may receive consecutive segments with the same start time
-				// so we only emit a segment if its following segment has a different start time
+
+            Interval lastInterval = new Interval(lastSegment.getStartTime(), lastSegment.getEndTime());
+            Interval curInterval = new Interval(curSegment.getStartTime(), curSegment.getEndTime());
+            Interval overlap = lastInterval.overlap(curInterval);
+            // check if last segment overlap with the cur segment
+            if(overlap != null){
+                // if som shorten the time span of the last segment, so that there is no overlap
+                lastSegment.setEndTime(overlap.getStart());
+            }
+            // emit the the last segment if it still exclusively covers some time interval after shortening
+
+            if(lastSegment.getEndTime().isAfter(lastSegment.getStartTime())){
 				this.createRecord()
 					.setData(lastSegment)
 					.setTimestamp(lastSegment.getEndTime())
 					.emit();
             }
 		}
-		lastSegment = record.getData();
+		lastSegment = curSegment;
 		checkpoint(record.getTimestamp());
 	}
  
